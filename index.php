d05d81d65f91d7685b3a6c5946a30ac0cd4eb736
Lucas Gomes <lulinux2109@gmail.com>
2020-06-03 15:13:55 -0300

    Alteração das pastas e códigos

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
-<?php
- echo 'Hello Word';
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Tcc</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<div class="container">
+		<div class="cabecalho">
+			<div>
+				<img class="logo" src="img/tbbt.jpg">
+			</div>
+			<nav>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Home</a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+</body>
+</html>
