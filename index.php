0222533e229c02f29397bb60e76d093dfda52a4d
Lucas Gomes <lulinux2109@gmail.com>
2020-06-05 21:42:18 -0300

    Alteração no menu e no head

--- index.php
+++ index.php
@@ -1,26 +1,31 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 	<head>
+		<!-- Metatags -->
 		<meta charset="utf-8">
-		// Adicionado o viewport, para deixar a página responsivel.
 		<meta name="viewport" content="width=device-width, initial-sacale=1.0">
-		// Alterado o titúlo da página.
-		<title> SkyLoop - Início </title>
+		<meta name="author" content="Lucas, Walisson, Wallace e Will">
+      	<meta name="description" content="Descrição do site isso é muito importante para o site">
+      	<meta name="keywords" content="sites, web, desenvolvimento">
+      	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<!-- Título da Página -->
+		<title> SkyLoop - Sua vida escolar na palma da sua mão ! </title>
+		<!-- Links da Página -->
 		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 	</head>
 	<body>
 		<div class="container">
 			<div class="cabecalho">
 				<div>
-					// Alteração na logo provisoria.
-					<img class="logo" src="img/sky.png">
+					<!--Alteração na logo provisoria.-->
+					<img class="logo" src="assets/img/sky.png">
 				</div>
 				<nav>
 					<ul>
-						// Alterações nos itens do menu.
-						<li><a href="inicio.php"> Início </a></li>
-						<li><a href="trabalhos.php"> Trabalhos </a></li>
-						<li><a href="sobre.php"> Sobre </a></li>					
+						<li><a href="#">Sobre</a></li>
+						<li><a href="#">Trabalhos</a></li>
+						<li><a href="#">Professores</a></li>
+						<li><a href="#">Contato</a></li>	
 					</ul>
 				</nav>
 			</div>
