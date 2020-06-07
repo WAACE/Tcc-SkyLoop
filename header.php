7573543c3e5b59cab43b3191531952a4898e172d
Lucas Gomes <lulinux2109@gmail.com>
2020-06-07 19:38:59 -0300

    Alterações nos arquivos de estilos e estruturação

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<!-- Metatags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-sacale=1.0">
+		<meta name="author" content="Lucas, Walisson, Wallace e Will">
+      	<meta name="description" content="Descrição do site isso é muito importante para o site">
+      	<meta name="keywords" content="sites, web, desenvolvimento">
+      	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<!-- Título da Página -->
+		<title> SkyLoop - Sua vida escolar na palma da sua mão! </title>
+		<!-- Links da Página -->
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+		<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500&display=swap" rel="stylesheet">
+	</head>
+	<body>
+		<div class="container">
+			<div class="cabecalho">
+				<div>
+					<!--Alteração na logo provisoria.-->
+					<img class="logo" src="assets/img/sky.png">
+				</div>
+				<nav>
+					<ul>
+						<li><a href="#">Sobre</a></li>
+						<li><a href="#">Trabalhos</a></li>
+						<li><a href="#">Professores</a></li>
+						<li><a href="#">Contato</a></li>	
+					</ul>
+				</nav>
+			</div>
+			<div class="clear"></div>
--- index.php
+++ index.php
@@ -1,47 +1,36 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-	<head>
-		<!-- Metatags -->
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-sacale=1.0">
-		<meta name="author" content="Lucas, Walisson, Wallace e Will">
-      	<meta name="description" content="Descrição do site isso é muito importante para o site">
-      	<meta name="keywords" content="sites, web, desenvolvimento">
-      	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<!-- Título da Página -->
-		<title> SkyLoop - Sua vida escolar na palma da sua mão! </title>
-		<!-- Links da Página -->
-		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-		<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500&display=swap" rel="stylesheet">
-	</head>
-	<body>
-		<div class="container">
-			<div class="cabecalho">
-				<div>
-					<!--Alteração na logo provisoria.-->
-					<img class="logo" src="assets/img/sky.png">
-				</div>
-				<nav>
-					<ul>
-						<li><a href="#">Sobre</a></li>
-						<li><a href="#">Trabalhos</a></li>
-						<li><a href="#">Professores</a></li>
-						<li><a href="#">Contato</a></li>	
-					</ul>
-				</nav>
-			</div>
-			<!-- São DIV's e Classes novas -->
-			<div class="ajuda">
-				<div>
-					<img class="imagem" src="assets/img/ajuda.jpg">
-				</div>
-				<div>
-					<h1 class="principal"> A ajuda está aqui a seu favor!</h1>
-				</div>
-			</div>
-			<div class="rodape">
-				Todos os direitos reservado				
-			</div>
+<?php 
+	require_once 'header.php';
+?>
+<main>
+	<div class="work-exatas">
+		<div class="box-work">
+			<h2>Trabalhos De Exatas</h2>
+			<figure>
+				<img src="assets/img/tbbt.jpg">
+				<figcaption>
+					<p>Trabalho -> Nome da matéria</p>
+				</figcaption>
+			</figure>
 		</div>
-	</body>
-</html>
+	</div>
+
+	<div class="work-biologicas">
+		
+	</div>
+
+	<div class="work-humanas">
+		<!-- #001 -->
+		<div class="box-work">
+			<h2>Trabalhos De Humanas</h2>
+			<figure>
+				<img src="assets/img/tbbt.jpg">
+				<figcaption>
+					<p>Trabalho -> Nome da matéria</p>
+				</figcaption>
+			</figure>
+		<!-- Fim Trabalhos -->
+	</div>
+</main>	
+<?php 
+	require_once 'footer.php';
+?>		
