23efc6e023a9b2e92abcfd9afcf397adcf0bfbee
WAACE <wallpower909090@gmail.com>
2020-07-13 22:12:56 -0300

    Alteração na página trabalhos

---
+++ add_agendamentos.php
@@ -0,0 +1,120 @@
+<?php 
+	include 'header.php';
+?>
+	<form action="#" method="POST">
+		<center>
+			<table>
+				<tr>
+					<td>
+						<p>Título</p>
+					</td>
+					<td>
+						<input type="text" name="nome-do-agendamento" placeholder="ex: Atividade avaliativa">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<p>Disciplina</p>
+					</td>
+					<td>
+						<div class="select-disciplina">
+							<select name="disciplina">
+								<option>Selecione</option>
+								<option value="bio">Biologia</option>
+								<option value="ds">DS</option>
+								<option value="edf">Educação Física</option>
+								<option value="filo">Filosofia</option>
+								<option value="fisc">Física</option>
+								<option value="geo">Geografia</option>
+								<option value="hist">História</option>
+								<option value="ing">Inglês</option>
+								<option value="pt">Língua Portuguesa</option>
+								<option value="mat">Matemática</option>
+								<option value="pc">PC</option>
+								<option value="ptcc">PTCC</option>
+								<option value="qui">Química</option>
+								<option value="rcd">RCD</option>
+								<option value="soc">Sociologia</option>
+								<option value="tpm">TPM</option>
+							</select>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<p>Professor(a)</p>
+					</td>
+					<td>
+						<input type="text" name="professor" placeholder="ex: Della Cross">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<p>Data de emissão</p>
+					</td>
+					<td>
+						<p>
+							<input type="date" name="data-de-emissao">
+						</p>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<p>Data de entrega</p>
+					</td>
+					<td>
+						<input type="date" name="data-do-agendamento">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<p>Equipe</p>
+					</td>
+					<td>
+						<div class="select-disciplina">
+							<select name="equipe">
+								<option>Individual</option>
+								<option>Dupla</option>
+								<option>Trio</option>
+								<option>Quarteto</option>
+								<option>Mais de 5 pessoas</option>
+							</select>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						
+					</td>
+					<td>
+						<label>Mínima&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+						<label>Máxima</label>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<p>Nota</p>
+					</td>
+					<td>
+						<span>
+							<input type="number" name="nota-minima">&nbsp;
+							<input type="number" name="nota-minima">
+						</span>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<p>Descrição</p>
+					</td>
+					<td>
+						<textarea name="descricao" placeholder=" Aqui você pode digitar informações adicionais ao agendamento"></textarea>
+					</td>
+				</tr>
+			</table>
+			<input class="botoes" type="submit" name="cadastrar-agendamento" value="Cadastrar">
+			<input class="botoes" type="reset" name="limpar" value="Limpar">
+		</center>
+	</form>
+<?php 
+	require_once 'footer.php';
+?>
--- trabalhos.php
+++ trabalhos.php
@@ -1,6 +1,15 @@
 <?php 
 	require_once 'header.php';
 ?>
+	<center>
+		<span>
+			<a href="add_agendamentos.php">Adicionar agendamento</a>
+		</span>
+		<span>
+			<a href="add_trabalhos.php">Adicionar trabalhos</a>
+		</span>
+	</center>
 <?php 
 	require_once 'footer.php';
 ?>	
+	
